c089c38ba91c57a88be7f4c32f1a95e0edb9e719
Raj Bariah <raj.uni@outlook.com>
2020-06-04 15:04:39 +0100

    Updates

---
+++ precache-manifest.37dfeaf2d60565b7567237f386cf2b58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62ca5c6c8355b6f3966e1fb8677c2ca8",
+    "url": "/dualathalon/index.html"
+  },
+  {
+    "revision": "ee2a3e8440754c2100b6",
+    "url": "/dualathalon/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "40df61aef44bdb81e9d2",
+    "url": "/dualathalon/static/js/2.f04e8c7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dualathalon/static/js/2.f04e8c7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee2a3e8440754c2100b6",
+    "url": "/dualathalon/static/js/main.6f1c9215.chunk.js"
+  },
+  {
+    "revision": "d80fdd6ee4e7b46d4aee",
+    "url": "/dualathalon/static/js/runtime-main.c7a6a33d.js"
+  }
+]);
